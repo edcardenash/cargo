15daa5fa223d2cff2d55db3ca22fe40ac11b2729
Enrique Altamirano <enrique.altamiros@gmail.com>
2022-12-30 01:53:49 -0300

    Creado el controllador de freights, forma basica y agregada la vista index

---
+++ app/controllers/freights_controller.rb
@@ -0,0 +1,47 @@
+class FreightsController < ApplicationController
+  def index
+    @freights = Freight.all
+  end
+
+  def show
+  end
+
+  def new
+    @freights = Freight.new
+  end
+
+  def create
+    @freight = Freight.new(freights_params)
+    if @freight.save
+      redirect_to freight_path(@freight), notice: 'Freight was successfully created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @freight.update(freights_params)
+      redirect_to @freight, notice: 'Freight has been updated'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @freight.destroy
+    redirect_to freight_url, notice: 'Freight has been deleted'
+  end
+
+  private
+
+  def freights_params
+    params.require(:freight).permit(:start_latitude, :start_longitude, :end_latitude, :end_longitude, :description, :start_date, :receiver_name, :receiver_phone, :round_trip)
+  end
+
+  def set_freight
+    @freight = Freight.find_by_id(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   resources :zones
   resources :cities
   resources :countries
+  resources :freights
 end
