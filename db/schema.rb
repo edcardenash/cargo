c6c4cc9bf6ea7065362f1f681eccd0640093d305
Kevin Mackinlay <kemack83@gmail.com>
2023-01-12 14:38:29 -0300

    cambios zone_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema[7.0].define(version: 2023_01_07_185407) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_01_11_002447) do
->>>>>>> master
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_001537) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -137,10 +133,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_002447) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "city_id", null: false
-    t.boolean "other_regions"
-    t.boolean "other_cities"
-    t.index ["city_id"], name: "index_vehicles_on_city_id"
     t.index ["user_id"], name: "index_vehicles_on_user_id"
   end
 
@@ -154,6 +146,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_002447) do
   add_foreign_key "reviews", "quotes"
   add_foreign_key "reviews", "users"
   add_foreign_key "users", "cities"
-  add_foreign_key "vehicles", "cities"
   add_foreign_key "vehicles", "users"
 end
