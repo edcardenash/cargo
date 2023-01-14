8e69a9a9d9d5111b035e16b90425c46f24a382ab
Enrique Altamirano <enrique.altamiros@gmail.com>
2023-01-14 17:33:18 -0300

    mejorando freights

--- app/controllers/freights_controller.rb
+++ app/controllers/freights_controller.rb
@@ -7,25 +7,26 @@ class FreightsController < ApplicationController
 
   def show
     @quote = Quote.new
+    @freight = Parking.find(params[:id])
     @markers = @freight.geocode.map do |freight|
       {
-        start_latitude: @freight.start_latitude,
-        start_longitude: @freight.start_longitude,
-        end_latitude: @freight.end_latitude,
-        end_longitude: @freight.end_longitude,
+        start_address: @freight.start_latitude,
+        end_address: @freight.start_longitude,
         info_window: render_to_string(partial: "info_window", locals: { freight: @freight })
       }
     end
   end
 
   def new
-    @freights = Freight.new
+    @freight = Freight.new
   end
 
   def create
     @freight = Freight.new(freights_params)
+    @freight.user = current_user
+    @freight.user_id = current_user.id
     if @freight.save
-      redirect_to freight_path(@freight), notice: 'Freight was successfully created'
+      redirect_to @freight, notice: 'Freight was successfully created'
     else
       render :new, status: :unprocessable_entity
     end
@@ -50,7 +51,7 @@ class FreightsController < ApplicationController
   private
 
   def freights_params
-    params.require(:freight).permit(:start_latitude, :start_longitude, :end_latitude, :end_longitude, :description, :start_date, :receiver_name, :receiver_phone, :round_trip)
+    params.require(:freight).permit(:start_address, :end_address, :description, :start_date, :receiver_name, :receiver_phone, :round_trip)
   end
 
   def set_freight
