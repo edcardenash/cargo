4ca21ac72b59d5e4874cb23e9ad04ef3d12adbb9
Edgar Cardenas <edcardenash@gmail.com>
2023-01-17 23:57:46 -0300

    quotes controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :vehicles do
-   # resources :freights, only: %i[vehicle_request]
-  end
+  resources :vehicles
   resources :freights do
     resources :quotes
   end
   resources :reviews
+  resources :quotes
   get 'vehicles/:id/freights/vehicle_request', to: 'vehicles#vehicle_request', as: 'vehicle_request'
 end
