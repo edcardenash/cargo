f5c16be867ae6adf453a4434a2b9f530e528a451
Edgar Cardenas <edcardenash@gmail.com>
2023-01-21 15:23:59 -0300

    navbar change

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="navbar"
 export default class extends Controller {
   connect() {
+    console.log(this.element)
   };
 
   updateNavbar() {
