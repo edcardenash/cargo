2902b1451937fed21789aefbf8207297b69e969b
Edgar Cardenas <edcardenash@gmail.com>
2023-01-21 09:38:38 -0300

    updating quotes

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -15,8 +15,9 @@ class QuotesController < ApplicationController
     @quote = Quote.new(quote_params)
     authorize @quote
     @quote.freight = @freight
+    @quote.user_id = current_user.id
     if @quote.save
-      redirect_to freight_quotes_path, notice: 'Quote was successfully created'
+      redirect_to freight_path(@freight), notice: 'Quote was successfully created'
     else
       render :new, status: :unprocessable_entity
     end
@@ -44,7 +45,7 @@ class QuotesController < ApplicationController
   private
 
   def quote_params
-    params.require(:quote).permit(:amount, :comment, :vehicle_id, :freight_id)
+    params.require(:quote).permit(:amount, :comment, :vehicle_id, :freight_id, :user_id)
   end
 
   def set_quote
