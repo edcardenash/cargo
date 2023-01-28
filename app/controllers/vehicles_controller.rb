b8bd90a44fa29fc4b51722c179ad0577da7cf9e7
Edgar Cardenas <edcardenash@gmail.com>
2023-01-28 16:50:14 -0300

    sorting issue on home page solved

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -37,6 +37,7 @@ class VehiclesController < ApplicationController
     @vehicle = Vehicle.new(vehicle_params)
     @vehicle.user = current_user
     @vehicle.user_id = current_user.id
+    @vehicle.rating = 5.0
     authorize @vehicle
     if @vehicle.save
       redirect_to vehicle_path(@vehicle), notice: "Vehículo agregado correctamente."
