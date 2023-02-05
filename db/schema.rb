a33efbde46038dc3ad934b22dd51a49835364854
Edgar Cardenas <edcardenash@gmail.com>
2023-02-05 15:10:49 -0300

    adding front logic to change tabs if quotes are pending, approved or rejected

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_03_014832) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_174038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,7 +96,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_03_014832) do
   end
 
   create_table "quotes", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 0
     t.integer "amount"
     t.text "comment"
     t.bigint "vehicle_id", null: false
