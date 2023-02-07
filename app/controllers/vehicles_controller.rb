d23aac71dc670c467c97abff6bb9311276238848
Edgar Cardenas <edcardenash@gmail.com>
2023-02-06 22:50:44 -0300

    refactoring vehicle controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,30 +3,21 @@ class VehiclesController < ApplicationController
   before_action :set_vehicle, only: [:show, :edit, :update, :destroy]
 
   def index
-    if params[:query].present?
-      @vehicles = policy_scope(Vehicle.global_search(params[:query]))
-    else
-      @vehicles = policy_scope(Vehicle)
-    end
+    @vehicles = policy_scope(Vehicle.global_search(params[:query])) if params[:query].present?
+    @vehicles ||= policy_scope(Vehicle)
 
     respond_to do |format|
       format.html
-      format.text { render partial: "shared/list_vehicles", locals: {vehicles: @vehicles}, formats: [:html] }
+      format.text { render partial: "shared/list_vehicles", locals: { vehicles: @vehicles }, formats: [:html] }
     end
   end
 
   def show
     @vehicle = Vehicle.find(params[:id])
     @review = Review.new
-    if @vehicle.quotes.any?
-      @vehicle.quotes.each do |quote|
-        @freights_ids = []
-        @freights_ids << quote.freight_id
-      end
-      @freights_ids.each do |id|
-        @freights = []
-        @freights << Freight.find(id)
-      end
+    quotes = @vehicle.quotes
+    if quotes.any?
+      @freights = quotes.map { |quote| Freight.find(quote.freight_id) }
     end
     authorize @vehicle
   end
@@ -40,12 +31,10 @@ class VehiclesController < ApplicationController
 
   def create
     @vehicle = Vehicle.new(vehicle_params)
-    @vehicle.user = current_user
-    @vehicle.user_id = current_user.id
-    @vehicle.rating = 5.0
+    @vehicle.assign_attributes(user: current_user, user_id: current_user.id, rating: 5.0)
     authorize @vehicle
     if @vehicle.save
-      redirect_to vehicle_path(@vehicle), notice: "Vehículo agregado correctamente."
+      redirect_to @vehicle, notice: "Vehículo agregado correctamente."
     else
       render :new, status: :unprocessable_entity
     end
